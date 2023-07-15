a8d235f4f7524e3889e7cb76b19898dce8fef8ba
Desmond Karani <karanidesmond@proton.me>
2023-07-15 16:10:07 +0300

    practice

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - prints hello guys followed by a new line
+*
+* Return: A return of 0 is always a success
+*/
+int main(void)
+
+{
+	printf("Hello guys, tsup!\n");
+	return (0);
+}
