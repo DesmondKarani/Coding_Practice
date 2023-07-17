299d6385a2962bf92d43dd200f37fb8c53ee5bb9
Desmond Karani <karanidesmond@proton.me>
2023-07-17 11:34:46 +0300

    learning about char 🥹🥹🥹💯

--- pract.c
+++ pract.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-float ghasia(int g, int d, float e)
+float ghasia(int g, char d, float e)
 {
 	float nongwe = (g * d) / e;
 	return nongwe;
@@ -7,7 +7,7 @@ float ghasia(int g, int d, float e)
 int main(void)
 {
 	float matokeo;
-	matokeo = ghasia(10, 5, 25.55);
-	printf("If you multiply ten by five, and then the result you divide by 25.55, the answer is %.4f\n", matokeo);
+	matokeo = ghasia(10, 'a', 25.55);
+	printf("If you multiply ten by the value of char 'a', and then the result you divide by 25.55, the answer is %.4f\n", matokeo);
 	return 0;
 }
