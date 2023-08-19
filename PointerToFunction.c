11628b83fb5caebb865fcf4d165429a1c71f880e
Desmond <karanidesmond@protonmail.ch>
2023-08-19 12:42:43 +0300

    function Pointers

--- PointerToFunction.c
+++ PointerToFunction.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
+int summation(int x, int y);
 int main(void)
 {
+	int result;
+	int (*SummationPointer)(int, int);
 
+	/* *
+	 * Please note, in the value assignement of function pointers, you
+	 * can just pass the name of the function without the & since that 
+	 * name represent the initial address of the function.
+	 */
+	SummationPointer = &summation;
+
+	result = SummationPointer(10, 10);
+
+	printf("Summing 10 and 10 yields %d", result);
+
+	return 0;
+
+}
+int summation(int x, int y)
+{
+	return x + y;
 }
